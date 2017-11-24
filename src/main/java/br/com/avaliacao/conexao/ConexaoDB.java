d348e189b7eae3a82af449bbab4d210d8e3138db
marcosbras <marcosbras@hotmail.com>
2017-11-24 20:26:47 -0200

    tela usuario

--- src/main/java/br/com/avaliacao/conexao/ConexaoDB.java
+++ src/main/java/br/com/avaliacao/conexao/ConexaoDB.java
@@ -10,10 +10,15 @@ public class ConexaoDB {
 		
 		try {
 			
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/dbteste", "root","");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			
-		} catch (SQLException e) {
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/dbteste", "root","UTSETaoiom");
 			
+		} catch (SQLException e) {			
+			
+			throw new RuntimeException(e);
+			
+		} catch (ClassNotFoundException e) {
 			
 			throw new RuntimeException(e);
 		}
--- src/main/java/br/com/avalicao/controller/UsuarioController.java
+++ src/main/java/br/com/avalicao/controller/UsuarioController.java
@@ -8,17 +8,124 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//http://localhost:8080/Avaliacao/UsuarioController.do
+import br.com.avaliacao.entidades.Usuario;
+import br.com.avaliacao.entidadades.dao.UsuarioDAO;
+
+/**
+ * 
+ * @author marcosbras
+ *
+ *         QueryString:
+ *
+ *         http://localhost:8080/Avaliacao/UsuarioController.do
+ *
+ *
+ */
+
 @WebServlet("/UsuarioController.do")
-public class UsuarioController extends HttpServlet{
+public class UsuarioController extends HttpServlet {
 
-@Override
-protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	System.out.println("Chamou!");
-}
+	/**
+	 * Ao iniciar o servlet
+	 */
+	public UsuarioController() {
+		System.out.println("Construtor...");
+	}
 
+	/**
+	 * O Init é chamado apos a construção do objeto
+	 */
 
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
 
-   
-   
+		System.out.println("init...");
+	}
+
+	
+	/**
+	 * Aqui a requisição post é utilizada para cadastrar ou alterar usuário,
+	 * se o cliente envia id será feita uma alteração caso contratário 
+	 * será realizada um inserção no banco
+	 * 
+	 */
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		Usuario usuario = new Usuario();
+
+		if (req.getParameter("id")!=null){
+		  
+			usuario.setId(Integer.parseInt(req.getParameter("id")));
+			
+		}
+		
+
+		if (req.getParameter("cfun")!=null){
+			  
+			usuario.setId(Integer.parseInt(req.getParameter("cfun")));
+			
+		}
+		
+		usuario.setLogin(req.getParameter("login"));
+		usuario.setEmail(req.getParameter("email"));
+		usuario.setSenha(req.getParameter("senha"));
+		
+		
+		UsuarioDAO usuarioDao = new UsuarioDAO();
+		usuarioDao.salvar(usuario);
+		
+		resp.getWriter().print("Salvo com sucesso!");
+
+		
+
+	}
+
+	/**
+	 * doGet é chamado toda vez que ocorre alguma requisição requisita dados do
+	 * tomcat
+	 * 
+	 * Aqui a requisição Get é utililizada para excluir um registro
+	 * 
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		
+		if(req.getParameter("id")!=null){
+		
+			Usuario usuario = new Usuario();
+			
+			usuario.setId(Integer.parseInt(req.getParameter("id")));
+			
+			UsuarioDAO usuarioDao = new UsuarioDAO();
+			
+			if (usuarioDao.BuscaUsuarioPorId(usuario.getId())!= null){
+				
+				usuarioDao.excluir(usuario);
+				
+				resp.getWriter().print("Exclusão realizada com sucesso!");	
+				
+			}else{
+				
+				resp.getWriter().print("Id do usuário não existe na base de dados");
+				
+			}
+			
+		}
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
 }
